a64899295b3bc021adffccd3355a8035d45de19d
SarojaRk <129292821+SarojaRk@users.noreply.github.com>
2023-04-14 00:52:03 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -19,3 +19,32 @@ resource "aws_lb_target_group" "albtg" {
   protocol = "HTTP"
   target_type = "instance"
   vpc_id   = aws_vpc.main.id
+
+  health_check {    
+    healthy_threshold   = 3    
+    unhealthy_threshold = 10    
+    timeout             = 5    
+    interval            = 10    
+    path                = "/"    
+    port                = 80  
+  }
+}
+
+resource "aws_lb_target_group_attachment" "front_end" {
+  target_group_arn = aws_lb_target_group.albtg.arn
+  target_id        = aws_instance.web[count.index].id
+  port             = 80
+  count = 2
+}
+
+//Listener
+resource "aws_lb_listener" "albl" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.albtg.arn
+  }
+}
